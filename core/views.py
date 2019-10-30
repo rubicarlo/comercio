3981ed4184808614ad413edd917dbea1de3ae90c
rubicarlo <rubicarlo.95@hotmail.com>
2019-10-30 15:19:13 +0100

    Viva el legitimo rey

--- core/views.py
+++ core/views.py
@@ -4,8 +4,8 @@ from .models import Item
 
 def item_list(request):
 
-    context={
-        'items':Item.objects.all()
+    context = {
+        'items': Item.objects.all()
     }
 
-    return render(request, "item_list.html",context)
+    return render(request, "home-page.html", context)
