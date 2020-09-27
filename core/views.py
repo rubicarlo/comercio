534b040ef22272bc1ed89faf92a3e2944c14df89
rubicarlo <rubicarlo.95@hotmail.com>
2020-09-27 12:27:28 +0200

    nuevo comienzo

--- core/views.py
+++ core/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 # Create your views here.
 
-def item_list(request):
+def products(request):
 
     context = {
         'items': Item.objects.all()
     }
 
-    return render(request, "home-page.html", context)
+    return render(request, "products.html", context)
+
+def checkout(request):
+        return render(request, "checkout.html")
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+def Home(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "home.html", context)
