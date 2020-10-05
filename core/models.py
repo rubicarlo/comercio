ce472b3f83c3f84ec984de736e72495e47a8887f
rubicarlo <rubicarlo.95@hotmail.com>
2020-10-05 12:13:41 +0200

    nuevo comienzo

--- core/models.py
+++ core/models.py
@@ -2,9 +2,23 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('S' , 'shirt'),
+    ('SW' , 'Sport wear'),
+    ('OW' , 'outwear'),
+)
+LABEL_CHOICES = (
+    ('P','primary'),
+    ('S','secondary'),
+    ('D','danger'),
+)
+
 class Item(models.Model):
     title=models.CharField(max_length=100)
     price = models.FloatField()
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    label=models.CharField(choices=LABEL_CHOICES,max_length=1)
 
     def __str__(self):
         return self.title
@@ -24,4 +38,3 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
-
